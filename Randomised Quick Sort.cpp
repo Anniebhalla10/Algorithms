21775af1bd9dbc70d4999cd5029b8c2e9c819801
Annie Bhalla <anniebhalla2001@gmail.com>
2021-03-04 14:17:41 +0530

    Update Randomised Quick Sort.cpp

--- Randomised Quick Sort.cpp
+++ Randomised Quick Sort.cpp
@@ -8,6 +8,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int comp=0;
+
+void value(int a[],int s){
+    for (int i=0; i<s; i++) {
+        a[i]=rand()%s;
+        cout<<a[i]<<endl;
+    }
+}
+
 int partition(int A[],int lb,int ub)
 {
 	int start=lb;
@@ -16,13 +25,20 @@ int partition(int A[],int lb,int ub)
 	while(start<end)
 	{
 		while(A[start]<=pivot)
-		  start++;
+		{
+			start++;
+			comp++;
+		}
 		  
 		while(A[end]>pivot)
-		   end--;
+		   {
+		   	end--;
+		   	comp++;
+		   }
 		   
 		if(start<end)
 		{
+			comp++;
 			swap(A[start],A[end]);
 		}
 		
@@ -46,26 +62,29 @@ void quick_sort(int A[], int lb,int ub)
 		int loc = partition_random(A,lb,ub);
 		quick_sort(A,loc+1,ub);
 		quick_sort(A,lb,loc-1);
+			cout<<"\n Number of comparisons : "<<comp<<endl;
 	}
 }
 
 
 int main()
 {
+	char ch;
+	int ans;
 	int size;
-	cout<<"\n Enter the umber of elements ";
-	cin>>size;
-	int *A=new int[size];
-	cout<<"\n Enter the elments in the array ";
-	for(int i=0;i<size;i++)
-	{
-		cin>>A[i];
-	}
-	quick_sort(A,0,size-1);
-	cout<<endl;
-	for(int i=0;i<size;i++)
-	{
-		cout<<A[i]<<" ";
-	}
+	do{
+	       	cout<<"\n Enter the umber of elements ";		
+			cin>>size;
+			int *A=new int[size];
+			value(A,size);
+			// sort(A,A+size);     // used for best case
+			// sort(A,A+size,greater<int>());   // used for worst case
+			quick_sort(A,0,size-1);
+			comp=0;
+			cout<<"\n Do you wish to continue ";
+			cin>>ch;
+						
+		}while(ch='y' || 'Y');
+
 	return 0;
 }
